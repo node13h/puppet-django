ac947de236b1b0946ea225139dc89944ca10998b
Sergej Alikov <sergej.alikov@gmail.com>
2015-07-10 00:41:17 +0300

    Support adding users with keys, installing packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,10 @@
 class django (
   $apps               = {},
   $workers            = {},
-  $beats               = {},
+  $beats              = {},
+  $users              = {},
+  $keys               = {},
+  $packages           = {},
 
   $manage_supervisord = $django::params::manage_supervisord,
 
@@ -15,5 +18,24 @@ class django (
   create_resources(django::app, $apps)
   create_resources(django::celery::worker, $workers)
   create_resources(django::celery::beat, $beats)
+
+  $user_defaults = {
+    ensure     => present,
+    managehome => true,
+    shell      => '/bin/sh',
+  }
+
+  create_resources(user, $users, $user_defaults)
+
+  $key_defaults = {
+    ensure => present,
+    'type' => 'ssh-rsa',
+  }
+
+  create_resources(ssh_authorized_key, $keys)
+
+  package { $packages:
+    ensure => present,
+  }
 
 }
